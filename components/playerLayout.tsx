6406987d46fde7599856563b98062e85fd70cbd4
ayushi-srivastava18 <ayushi.srivastava18@student.amity.edu>
2022-12-26 20:33:50 +0530

    style : create sidebar (chakra ui items + icons)

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,10 +1,11 @@
 import { Box } from "@chakra-ui/layout";
+import { Sidebar } from "./sidebar";
 
 export const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" left="0" width="250px">
-        Sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {" "}
---
+++ components/sidebar.tsx
@@ -0,0 +1,22 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Center,
+  Divider,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibrary,
+  MdPlaylistAdd,
+  MdFavourite,
+} from "react-icons";
+
+export const Sidebar = () => {
+  return <div>Hello from sidebar</div>;
+};
