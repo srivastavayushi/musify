8415c477bb06ed34e9922a0124d2afef4b424d24
ayushi-srivastava18 <ayushi.srivastava18@student.amity.edu>
2022-12-26 21:08:28 +0530

    feature : added Menu items (sidebar)

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,4 @@
+import NextLink from 'next/link'
 import NextImage from "next/image";
 import {
   Box,
@@ -15,8 +16,40 @@ import {
   MdSearch,
   MdLibrary,
   MdPlaylistAdd,
-  MdFavourite,
-} from "react-icons";
+  MdFavorite,
+  MdLibraryMusic
+} from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
 
 export const Sidebar = () => {
   return (
@@ -30,6 +63,26 @@ export const Sidebar = () => {
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
