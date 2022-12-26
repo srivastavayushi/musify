c0cc7c566a16126d1a94b3afeeeef7bdb1518ade
ayushi-srivastava18 <ayushi.srivastava18@student.amity.edu>
2022-12-26 20:21:47 +0530

    code : Playlist container setup

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,10 +1,18 @@
-import {Box} from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
 
 export const PlayerLayout = ({ children }) => {
   return (
-    <Box>
-      Layout
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" left="0" width="250px">
+        Sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {" "}
+        {children}{" "}
+      </Box>
+      <Box position="absolute" bottom="0" left="0">
+        Player
+      </Box>
     </Box>
   );
 };
